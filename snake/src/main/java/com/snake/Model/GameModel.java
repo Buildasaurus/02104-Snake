d27f85addd485503f98d2a43b18a96e9c1784c7c
Kajsapixar <kajsa-2001@hotmail.com>
2024-01-02 21:13:21 +0100

    GameModel

--- snake/src/main/java/com/snake/Model/GameModel.java
+++ snake/src/main/java/com/snake/Model/GameModel.java
@@ -1,17 +1,61 @@
 package com.snake.Model;
 
 import java.util.Timer;
+import java.utlil.Arraylist;
+
 
 public class GameModel {
     Tile[][] board;
     Vector direction;
     Timer time;
-    void updateDirection(Vector _direction)
-    {
-        direction = _direction;
+    Vector snakePosition; // keep track of position
+    int rowCount;
+    int columnCount;
+
+    public GameModel(int rowCount, int columnCount) {
+
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        snakePosition = new Vector(row/2, column/2); //snake start position
+        direction = new Vector(rowCount, columnCount); //initializing direction
+
+        boolean startSquare = false;
     }
-    void nextState()
+    void updateDirection(Vector direction)
     {
-        //TODO implement this.
+        this.direction = direction;
     }
+    /**
+     * if inside board limit
+     * @param position
+     * @return
+     */
+    private boolean isValidMove(Vector position) 
+    {
+        return position.x >= 0 && position.x < rowCount &&
+               position.y >= 0 && position.y < columnCount;
+    }
+    Vector nextState()
+    {
+        Vector nextPosition = snakePosition.add(direction);
+
+        if (isValidMove(nextPosition)) {
+            return nextPosition();
+        }
+
+        if (isValidMove(nextPosition)) 
+        {
+            snakePosition = nextPosition;
+        }
+        else 
+        {
+            return null;
+        }
+    }
+    
+    public void setDirection(Vector direction)
+    {
+        this.direction = direction;
+    }
+
 }
