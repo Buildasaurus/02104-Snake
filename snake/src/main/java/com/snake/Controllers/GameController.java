cd859bf6bf02f44c53c16308b4b5ebf8ef5a6a91
manajuDK <manaju.dk@gmail.com>
2024-01-02 21:59:44 +0100

    gamecontroller esc handling and fixes

--- snake/src/main/java/com/snake/Controllers/GameController.java
+++ snake/src/main/java/com/snake/Controllers/GameController.java
@@ -3,7 +3,10 @@ package com.snake.Controllers;
 import java.util.Timer;
 import com.snake.Settings;
 import com.snake.Model.GameModel;
+import com.snake.Model.Vector;
 import com.snake.Views.GameView;
+
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.input.KeyEvent;
 
@@ -21,7 +24,7 @@ public class GameController implements IController
     public GameController(int rowCount, int columnCount)
     {
         this.view = new GameView(rowCount, columnCount, Settings.windowHeight, Settings.windowWidth);
-        this.model = new GameModel();
+        this.model = new GameModel(rowCount, columnCount);
         this.gameTimer = new Timer();
 
         view.setOnKeyPressed(this::handleKeyPressed);
@@ -32,25 +35,26 @@ public class GameController implements IController
         switch (key.getCode()) {
             case UP:
             case W:
-                //model.setDirection(new Vector(0, 1));
+                model.setDirection(new Vector(0, 1));
                 break;
 
             case DOWN:
             case S:
-                //model.setDirection(new Vector(0, -1));
+                model.setDirection(new Vector(0, -1));
                 break;
 
             case LEFT:
             case A:
-                //model.setDirection(new Vector(-1, 0));
+                model.setDirection(new Vector(-1, 0));
                 break;
 
             case RIGHT:
             case D:
-                //model.setDirection(new Vector(1, 0));
+                model.setDirection(new Vector(1, 0));
                 break;
 
-
+            case ESCAPE:
+                Platform.exit();
 
             default:
                 System.out.println("non functional key " + key.getCode() + " pressed");
