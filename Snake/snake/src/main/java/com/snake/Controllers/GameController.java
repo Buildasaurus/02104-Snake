a344b5cac51b3e6c6567b910ae9ad53f2435ec78
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-02 13:18:38 +0100

    GameController

--- Snake/snake/src/main/java/com/snake/Controllers/GameController.java
+++ Snake/snake/src/main/java/com/snake/Controllers/GameController.java
@@ -1,12 +1,35 @@
 package com.snake;
 
 import java.io.IOException;
+import javax.swing.Icon;
 import javafx.fxml.FXML;
 
-public class GameController {
-    Gam
+public class GameController implements IController
+{
+    GameView view;
+    GameModel model;
+    Timer gameTimer;
+    Vector direction;
+
+    public Parent getView()
+    {
+        // TODO
+        return null;
+    }
+
+    public void GameController()
+    {
+
+    }
+
+    void update(KeyEvent keyPressed)
+    {
+
+    }
+
     @FXML
-    private void switchToSecondary() throws IOException {
+    private void switchToSecondary() throws IOException
+    {
         App.setRoot("secondary");
     }
 }
