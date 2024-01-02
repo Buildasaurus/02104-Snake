448d78dd38759ef1d529af11ca436bc197ec3773
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-02 13:14:35 +0100

    Names in class diagram

--- Snake/snake/src/main/java/com/snake/Controllers/GameController.java
+++ Snake/snake/src/main/java/com/snake/Controllers/GameController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class GameController {
-
+    Gam
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
