b56d9b8737c418eeb7714b4f21b0497e13109a06
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-02 13:13:03 +0100

    GameModel and GameController
    also structure

--- Snake/snake/src/main/java/com/snake/Controllers/GameController.java
+++ Snake/snake/src/main/java/com/snake/Controllers/GameController.java
@@ -3,7 +3,7 @@ package com.snake;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class GameController {
 
     @FXML
     private void switchToSecondary() throws IOException {
---
+++ Snake/snake/src/main/java/com/snake/Model/GameModel.java
@@ -0,0 +1,13 @@
+public class GameModel {
+    Tile[][] board;
+    Vector direction;
+    Timer time;
+    void updateDirection(Vector _direction)
+    {
+        direction = _direction;
+    }
+    void nextState()
+    {
+        //TODO implement this.
+    }
+}
