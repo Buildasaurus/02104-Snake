99e20741b12a1b7282c16ba718167bff9974baa6
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-03 09:41:20 +0100

    Images

--- snake/src/main/java/com/snake/Model/Tile.java
+++ snake/src/main/java/com/snake/Model/Tile.java
@@ -1,11 +1,14 @@
 package com.snake.Model;
 
+import java.io.FileInputStream;
+import java.net.URL;
+import com.snake.App;
 import javafx.scene.image.Image;
 
 public class Tile
 {
-
     TileType tileType;
+    Image sprite;
 
     public Tile(TileType type)
     {
@@ -19,8 +22,24 @@ public class Tile
 
     public Image getImage()
     {
-        return null;
+        if (sprite == null)
+        {
+            try
+            {
+                String imageName = tileType.toString().toLowerCase();
+                URL url = App.class.getResource("/resources/" + imageName + ".png");
+
+                FileInputStream fis = new FileInputStream(imageName + ".png");
+                sprite = new Image(fis);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Image not found");
+            }
+        }
+        return sprite;
     }
+
 
     public boolean equals()
     {
