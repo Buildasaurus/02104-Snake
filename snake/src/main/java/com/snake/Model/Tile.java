50eb3726f799fe1da5ec75bc8f3d5024e9ab3643
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-03 11:12:01 +0100

    Image finder

--- snake/src/main/java/com/snake/Model/Tile.java
+++ snake/src/main/java/com/snake/Model/Tile.java
@@ -48,17 +48,61 @@ public class Tile
         return false;
     }
 
+    /**
+     * Returns one of eight values, straightright, straightup, straightdown, straightleft,
+     * turnright, turnup, turnleft, turndown Here the turns, eg turn up, is coming from the right,
+     * then going upwards, always 90 degrees. Assuming that the two directions aren't opposite of
+     * each other.
+     *
+     * @param firstDirection
+     * @param secondDirection
+     * @return
+     */
     private String getDirectionName(Vector firstDirection, Vector secondDirection)
     {
+        if (firstDirection.x == -secondDirection.x || firstDirection.y == -firstDirection.y)
+        {
+            System.out.println("opposite directions");
+        }
         int crossProduct = firstDirection.crossProduct(secondDirection);
         switch (crossProduct)
         {
-            case 1:
-                return "right";
+
             case 0:
-                return "up";
+                // They are parallel, so now we can figure out in which direction
+                if (firstDirection.x == 1 && firstDirection.y == 0)
+                    return "right";
+                else if (firstDirection.x == -1 && firstDirection.y == 0)
+                    return "left";
+                else if (firstDirection.x == 0 && firstDirection.y == 1)
+                    return "up";
+                else if (firstDirection.x == 0 && firstDirection.y == -1)
+                    return "down";
+                else
+                {
+                    System.out.println("vectors are weird, or something is wrong.");
+                    return "error";
+                }
             case -1:
-                return "left";
+                Vector temp = firstDirection;
+                firstDirection = secondDirection;
+                secondDirection = temp;
+            case 1:
+                // They are parallel, so now we can figure out in which direction
+                if (firstDirection.x == 1 && firstDirection.y == 0)
+                    return "turn_down";
+                else if (firstDirection.x == -1 && firstDirection.y == 0)
+                    return "turn_up";
+                else if (firstDirection.x == 0 && firstDirection.y == 1)
+                    return "turn_right";
+                else if (firstDirection.x == 0 && firstDirection.y == -1)
+                    return "turn_left";
+                else
+                {
+                    System.out.println("vectors are weird, or something is wrong.");
+                    return "error";
+                }
+
             default:
                 System.out.println("vectors are weird, or something is wrong.");
                 return "";
