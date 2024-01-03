c01ff06cca6920b39402ac87842df69902e06511
Lucia <lucia.little123@gmail.com>
2024-01-03 10:42:12 +0100

    vores ting

--- snake/src/main/java/com/snake/Model/GameModel.java
+++ snake/src/main/java/com/snake/Model/GameModel.java
@@ -10,15 +10,18 @@ public class GameModel {
     int rowCount;
     int columnCount;
 
+
     public GameModel(int rowCount, int columnCount) {
 
         this.rowCount = rowCount;
         this.columnCount = columnCount;
-        snakePosition = new Vector(rowCount/2, columnCount/2); //snake start position
+        Vector head = new Vector(rowCount/2, columnCount/2); //snake start position
+        Vector tail = new Vector(rowCount/2, columnCount/2 + 1);
         direction = new Vector(1, 0); //initializing direction as right
 
         boolean startSquare = false;
     }
+
     void updateDirection(Vector direction)
     {
         this.direction = direction;
@@ -33,15 +36,36 @@ public class GameModel {
         return position.x >= 0 && position.x < rowCount &&
                position.y >= 0 && position.y < columnCount;
     }
-    public void nextState()
+
+    public void nextState(Vector head, Vector tail)
     {
-        Vector nextPosition = snakePosition.add(direction);
+        Vector nextPosition = head.add(direction);
 
         if (isInRange(nextPosition))
         {
-            snakePosition = nextPosition;
+
             //Implement rest of next state logic, as we won't get out of bounds here.
+            if (board[nextPosition.x][nextPosition.y] == null){
+                board[head.x][head.y] = null;
+                //opdatere hoved
+                head = head.add(direction);
+                board[head.x][head.y] = new Tile();
+                //board[head.x][head.y].tileType = TileType.Snakehead;
+
+                board[tail.x][tail.y] = null;
+                //opdatere hale
+                tail = tail.add(direction);
+                board[tail.x][tail.y] = new Tile();
+                //board[tail.x][tail.y].tileType = TileType.Snaketail;
+            }
+            else {
+                gameOver();
+            }
+
         }
+        else {
+            gameOver();
+        }   
     }
 
     public void setDirection(Vector direction)
@@ -49,4 +73,8 @@ public class GameModel {
         this.direction = direction;
     }
 
+    public void gameOver(){
+        //game over
+    }
+    
 }
