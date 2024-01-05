fb3a8159173ff3b7cbe3163cd935c87912fc5b26
Lucia Little <lucia.little123@gmail.com>
2024-01-05 15:39:51 +0100

    slay gui og non resizable window

--- snake/src/main/java/com/snake/Views/MenuView.java
+++ snake/src/main/java/com/snake/Views/MenuView.java
@@ -1,17 +1,20 @@
 package com.snake.Views;
 
-
 import com.snake.Controllers.MenuController;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import java.net.URL;
 
 public class MenuView extends StackPane
 {
@@ -26,66 +29,40 @@ public class MenuView extends StackPane
 
     private void initialize()
     {
-        /*background image
-        URL url = getClass().getResource("/com/snake/Graphics/main.png");
-        BackgroundImage BI = new BackgroundImage(new Image(url.toString()), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        this.setBackground(new Background(BI));*/
-
-        //background color
-        this.setStyle("-fx-background-color: #D8BFD8");
-
-        //title
-        Label titleLabel = new Label("Snake");
-        titleLabel.setFont(Font.font("Arial", FontWeight.BOLD, 56));
-        setAlignment(titleLabel, Pos.TOP_CENTER);
-        setMargin(titleLabel, new Insets(100,0,0,0));
-        getChildren().add(titleLabel);
+        URL url = getClass().getResource("/com/snake/Graphics/titlescreen.png");
+        BackgroundImage backgroundImg = new BackgroundImage(new Image(url.toString()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, 
+        new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
+        Background background = new Background(backgroundImg);
+        setBackground(background);
+        //background image scaling
 
         //buttons
-        Button playButton = new Button("Play");
-        Button exitButton = new Button("Quit");
-
-        playButton.setFont(new Font(30));
-        exitButton.setFont(new Font(30));
+        Button playButton = new Button();
+        Button exitButton = new Button();
 
         playButton.setOnAction(controller::handlePlayButtonPressed);
         exitButton.setOnAction(controller::handleExitButtonPressed);
+
 
         buttonstyling(playButton);
         buttonstyling(exitButton);
+        
+        URL playUrl = getClass().getResource("/com/snake/Graphics/playbutton.png");
+        playButton.setGraphic(new ImageView(new Image(playUrl.toString())));
+        URL quitUrl = getClass().getResource("/com/snake/Graphics/quitbutton.png");
+        exitButton.setGraphic(new ImageView(new Image(quitUrl.toString())));
+        
+        playButton.setTranslateY(50);
+        getChildren().add(playButton);
 
-        VBox buttonspacing = new VBox(20);
-        buttonspacing.getChildren().addAll(playButton, exitButton);
-        getChildren().add(buttonspacing);
+        exitButton.setTranslateX(290);
+        exitButton.setTranslateY(215);
+        getChildren().add(exitButton);
     }
 
     private void buttonstyling(Button button){
-        button.setStyle("-fx-focus-color: transparent;-fx-background-color: transparent;-fx-cursor: hand; -fx-font-family: 'Arial'; -fx-font-size: 32");
+        button.setStyle("-fx-background-color: transparent; -fx-focus-color: transparent;-fx-cursor: hand; -fx-font-family: 'Arial'; -fx-font-size: 32");
     }
     
-
-    /* private void initialize() {
-        // Creating buttons
-        Button playButton = new Button("Play");
-        Button exitButton = new Button("Quit");
-
-        // Setting button fonts and styles
-        playButton.setFont(new Font(30));
-        exitButton.setFont(new Font(30));
-        playButton.setStyle("-fx-background-color: lightgreen; -fx-text-fill: black;");
-        exitButton.setStyle("-fx-background-color: orangered; -fx-text-fill: white;");
-
-        // Setting action handlers from MenuController
-        playButton.setOnAction(controller::handlePlayButtonPressed);
-        exitButton.setOnAction(controller::handleExitButtonPressed);
-
-        // Organizing buttons in a vertical layout
-        VBox buttonBox = new VBox(playButton, exitButton);
-
-        // Adding buttons to the StackPane
-        getChildren().add(buttonBox);
-        buttonBox.setAlignment(Pos.CENTER);
-    }
-     */
 }
 
