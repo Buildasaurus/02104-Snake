e208b08ed1cdad818403114b8547a02c4d92f64d
manajuDK <manaju.dk@gmail.com>
2024-01-05 14:21:27 +0100

    gamestate speed refactoring

--- Snake/src/main/java/com/snake/Model/GameState.java
+++ Snake/src/main/java/com/snake/Model/GameState.java
@@ -2,23 +2,17 @@ package com.snake.Model;
 
 public class GameState {
     private Tile[][] board;
-    private double speed;
     private double acceleration;
     private Snake[] players;
 
     public GameState(Tile[][] board, double speed, double acceleration, Snake[] players) {
         this.board = board;
-        this.speed = speed;
         this.acceleration = acceleration;
         this.players = players;
     }
 
     public Tile[][] getBoard() {
         return board;
-    }
-
-    public double getSpeed() {
-        return speed;
     }
 
     public double getAcceleration() {
