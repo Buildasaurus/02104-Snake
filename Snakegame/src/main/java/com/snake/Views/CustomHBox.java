0494aba3913ad9c2ffc3b18c55e3cee05f91253c
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-01-09 12:58:19 +0100

    Update CustomHBox.java

--- Snakegame/src/main/java/com/snake/Views/CustomHBox.java
+++ Snakegame/src/main/java/com/snake/Views/CustomHBox.java
@@ -16,12 +16,10 @@ public class CustomHBox extends HBox{
        setStyle("-fx-background-image: url('" + url.toExternalForm() + "'); -fx-background-color: transparent; -fx-background-attachment: fixed; -fx-background-position: 50% 50%; -fx-background-repeat: no-repeat");
        //setPrefSize(300.0, 130.0);
 
-        Region clip = new Region(); //en nod
-
         //marginaler för CustomHBox
-        Insets margins = new Insets(300); //Insets-objekt med önskade marginaler
-        setMargin(clip, margins); // Använd setMargin för att tillämpa marginaler på clip eller annan nod
-        //setMargin(getClip(), getInsets());
+        setPadding(new Insets(50,50,50,50)); //Insets-objekt med önskade marginaler
+
+        //setPrefSize(2000.0, 100.0);
     }
     
 }
