0173336c4b87eaacc042aca0f17314a69fa70de3
Kajsapixar <kajsa-2001@hotmail.com>
2024-01-10 15:56:19 +0100

    scaled image I NGV

--- Snakegame/src/main/java/com/snake/Views/SelectionButton.java
+++ Snakegame/src/main/java/com/snake/Views/SelectionButton.java
@@ -3,22 +3,48 @@ package com.snake.Views;
 import java.net.URL;
 
 import com.snake.OurButton;
+import com.snake.Settings;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class SelectionButton extends Button {
+
+    private double scaler = 6.0;
+
+    private double x = Settings.windowWidth/scaler;
+
+    private double y = Settings.windowHeight/scaler;
     
     public  SelectionButton(String buttonName) 
     {
         super(buttonName);
+        this.scaler = scaler;
+
+       
        URL url = OurButton.class.getResource("/com/snake/Graphics/smallplaybt.png");
-       setStyle("-fx-cursor: hand; -fx-background-image: url('" + url.toExternalForm() + "'); -fx-background-color: transparent; -fx-font-size: 20; -fx-background-attachment: fixed; -fx-background-position: 50% 50%; -fx-background-repeat: no-repeat");
-       setPrefSize(300.0, 130.0);
-  
+        Image buttonbackgroundimgfile = new Image(url.toString(), Settings.windowWidth/scaler, 0, true, false);
+        BackgroundImage buttonbackgroundimg = new BackgroundImage(buttonbackgroundimgfile, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+        new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
+        Background buttonbackground = new Background(buttonbackgroundimg);
+        setBackground(buttonbackground);
+        setStyle("-fx-cursor: hand; -fx-font-size: 50");
+        setPrefSize(Settings.windowWidth/scaler, Settings.windowHeight/scaler);
+       
+
+
+        
     }
+  
+    
     public void horizontally(Stage Stage) {
         
         // create a HBox 
